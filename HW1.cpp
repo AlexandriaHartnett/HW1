fb4c63ed632444f0a78311ab418756c5d7dd8f85
LewisCollum <lewiscollum4@gmail.com>
2021-01-26 22:39:37 -0500

    calculated total price

--- HW1.cpp
+++ HW1.cpp
@@ -4,5 +4,16 @@ int main() {
   std::string name;
   std::cout << "Product Name: ";
   std::cin >> name;
+
+  int price;
+  std::cout << "Wholesale Price: ";
+  std::cin >> price;
+
+  int quantity; 
+  std::cout << "Quantity: ";
+  std::cin >> quantity;
+
+  int totalPrice = price * quantity;
+  std::cout << "Total Price: " << totalPrice << std::endl;
   return 0;
 }
